729c34996301b3df41b160b6a819110aa7e1f601
marufthenoob <maruf.taskcodigital@gmail.com>
2024-11-24 16:25:53 +0600

    logos updated

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex max-w-3xl text-center flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert max-w-[90%] w-[400px] mx-auto"
+          className="max-w-[90%] w-[400px] mx-auto"
           src="/website-lagbo.webp"
           alt="dipjol"
           width={400}
